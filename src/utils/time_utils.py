5f47e8499c0a820ab12eee863365321e12979f77
dong-park <rakddh@gmail.com>
2024-10-09 16:56:54 +0900

    feat: time관련 함수 추가

---
+++ src/utils/time_utils.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+import pytz
+
+
+def get_utc_now():
+    return datetime.now(pytz.UTC)
+
+
+def convert_to_utc(dt, timezone):
+    local = pytz.timezone(timezone)
+    local_dt = local.localize(dt, is_dst=None)
+    return local_dt.astimezone(pytz.UTC)
+
+
+def convert_to_local(utc_dt, timezone):
+    local = pytz.timezone(timezone)
+    return utc_dt.astimezone(local)
+
+
+def format_datetime(dt):
+    return dt.isoformat()

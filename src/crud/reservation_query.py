dd87c639925a9fba855c35d9d7fb629be1a6cff9
dong-park <rakddh@gmail.com>
2024-10-09 20:41:22 +0900

    feat: reservation_crud 긴 쿼리 reservation_query 소스 이동

--- src/crud/reservation_query.py
+++ src/crud/reservation_query.py
@@ -1,10 +1,35 @@
 from datetime import datetime
+from typing import List
 
 from sqlalchemy import func, and_
 from sqlalchemy.orm import Session
 
-from src.models import ExamSchedule, Reservation
+from src.models import ExamSchedule, Reservation, User
 from src.schemas.reservation import AvailableTimeSchema
+
+
+def get_user_reservations(db: Session, user_id: int, page: int = 0, limit: int = 100) -> List[Reservation]:
+    query = db.query(
+        Reservation.reservation_id,
+        Reservation.exam_id,
+        Reservation.is_confirmed,
+        Reservation.num_participants,
+    )
+
+    if user_id is not None:
+        query = query.filter(Reservation.user_id == user_id)
+
+    if user_id is None:
+        query = query.join(User, Reservation.user_id == User.user_id)
+        query = query.add_columns(
+            User.user_id,
+            User.username,
+            User.email
+        )
+
+    offset = (page - 1) * limit
+    query_result = query.offset(offset).limit(limit).all()
+    return query_result
 
 
 def get_available_times(db: Session, start_date: datetime, end_date: datetime):
@@ -32,5 +57,6 @@ def get_available_times(db: Session, start_date: datetime, end_date: datetime):
         exam_id=result.exam_id,
         start_time=result.start_time,
         max_capacity=result.max_capacity,
-        reserved_participants=result.reserved_participants
+        reserved_participants=result.reserved_participants,
+        available_slots=result.max_capacity - result.reserved_participants,
     ) for result in query_result]
